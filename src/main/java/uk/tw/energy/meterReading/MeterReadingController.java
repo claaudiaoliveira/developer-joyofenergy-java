54164fd14165771388519d84aa890c78b0e7c5af
Claudia de Oliveira Ramos <cmplg20@gmail.com>
2022-06-07 12:43:03 -0300

    reempacotando utilizando organização de uso: classes ElectricityReading

--- src/main/java/uk/tw/energy/meterReading/MeterReadingController.java
+++ src/main/java/uk/tw/energy/meterReading/MeterReadingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import uk.tw.energy.domain.ElectricityReading;
+import uk.tw.energy.Electricity.ElectricityReading;
 
 @RestController
 @RequestMapping("/readings")
